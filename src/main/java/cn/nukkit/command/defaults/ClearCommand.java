89d80bb36adf89adff3e24e26fa8218dcb512c50
Egor Tunekov <69630128+olovink@users.noreply.github.com>
2022-04-24 17:11:03 +0300

    Update ClearCommand.java
    Add permissions and description for CleaeCommand

--- src/main/java/cn/nukkit/command/defaults/ClearCommand.java
+++ src/main/java/cn/nukkit/command/defaults/ClearCommand.java
@@ -1,1 +1,26 @@
-// soon
+package cn.nukkit.command.defaults;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
+import cn.nukkit.lang.TranslationContainer;
+import cn.nukkit.utils.TextFormat;
+
+public class ClearCommand extends VanillaCommand {
+
+    public GamemodeCommand(String name) {
+        super(name, "Clear your inventory", "/clear",
+                new String[]{"clear"});
+        this.setPermission("nukkit.command.clear;");
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
+            return false;
+        }
+      // CODE 
+}
