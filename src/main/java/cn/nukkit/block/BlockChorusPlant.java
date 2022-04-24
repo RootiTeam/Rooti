6a9361d0286802a0c20436fe2c41e1b831c8bd64
ddosnikgit <ddosnikofficial@gmail.com>
2022-04-24 12:41:46 +0300

    add (chorusplant, chorusflower), update libraries in pom.xml

---
+++ src/main/java/cn/nukkit/block/BlockChorusPlant.java
@@ -0,0 +1,54 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemID;
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.utils.BlockColor;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BlockChorusPlant extends BlockTransparent {
+
+	public BlockChorusPlant() {
+		this(0);
+	}
+
+    public BlockChorusPlant(int meta) {
+        super(meta);
+    }
+
+    @Override
+    public int getId() {
+    	return CHORUS_PLANT;
+    }
+
+    @Override
+    public String getName() {
+    	return "Chorus Plant";
+    }
+
+    @Override
+    public double getHardness() {
+    	return 0.4;
+    }
+
+    @Override
+    public double getResistance() {
+    	return 2;
+    }
+
+    @Override
+    public int getToolType() {
+    	return ItemTool.TYPE_NONE;
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+    	return ThreadLocalRandom.current().nextBoolean() ? new Item[]{Item.get(ItemID.CHORUS_FRUIT, 0, 1)} : new Item[0];
+    }
+
+    @Override
+    public BlockColor getColor() {
+    	return BlockColor.PURPLE_BLOCK_COLOR;
+    }
+}
