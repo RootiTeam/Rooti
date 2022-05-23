9372688b573f0fe5c90e73bb335982446b01b170
Egor Tunekov <69630128+olovink@users.noreply.github.com>
2022-05-23 16:42:24 +0700

    Update Level.java

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -1987,7 +1987,7 @@ public class Level implements ChunkManager, Metadatable {
             Entity[] entities = this.getCollidingEntities(hand.getBoundingBox());
             int realCount = 0;
             for (Entity e : entities) {
-                if (e instanceof EntityArrow || e instanceof EntityItem || (e instanceof Player && ((Player) e).isSpectator())) {
+                if (e instanceof EntityArrow || e instanceof EntityItem || (e instanceof Player && ((Player) e).isSpectator()) || player == e) {
                     continue;
                 }
                 ++realCount;
