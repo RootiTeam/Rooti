11f536153a924ad498d5784271e0b34a220d7ed1
Egor Tunekov <69630128+olovink@users.noreply.github.com>
2022-05-23 16:29:25 +0700

    Update HurtArmorPacket.java

--- src/main/java/cn/nukkit/network/protocol/HurtArmorPacket.java
+++ src/main/java/cn/nukkit/network/protocol/HurtArmorPacket.java
@@ -10,22 +10,29 @@ public class HurtArmorPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.HURT_ARMOR_PACKET;
 
+    public int cause;
     public int health;
+    public long armorSlots;
 
     @Override
     public void decode() {
-
+        this.cause = getVarInt();
+        this.health = getVarInt();
+        this.armorSlots = getUnsignedVarLong();
     }
 
     @Override
     public void encode() {
         this.reset();
+        this.putVarInt(this.cause);
         this.putVarInt(this.health);
+        this.putUnsignedVarLong(this.armorSlots);
     }
 
     @Override
     public byte pid() {
         return NETWORK_ID;
+        
     }
 
 }
